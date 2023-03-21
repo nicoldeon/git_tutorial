c7da4a97a3ac529003a62bccc481f16874af009e
onhauyennhi <onhauyennhi254@gmail.com>
2023-03-21 15:17:39 +0700

    test stash 2

--- helloB.py
+++ helloB.py
@@ -4,11 +4,9 @@ print("Helo")
 print("Helo")
 
 
-print("Helo")
-print("Helo")
-print("Helo")
-print("Helo")
-print("Helo")
-print("Helo")
-
-
+print("123")
+print("123")
+print("123")
+print("123")
+print("123")
+print("123")
