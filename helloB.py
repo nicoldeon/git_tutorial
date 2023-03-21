caea5db7e0f7d0a96ca195d76326ab214269f148
nicoldeon <128443755+nicoldeon@users.noreply.github.com>
2023-03-21 15:15:39 +0700

    Update helloB.py

--- helloB.py
+++ helloB.py
@@ -2,7 +2,7 @@ print("Helo")
 print("Helo")
 print("Helo")
 print("Helo")
-<<<<<<< Updated upstream
+
 
 print("Helo")
 print("Helo")
@@ -11,4 +11,4 @@ print("Helo")
 print("Helo")
 print("Helo")
 =======
->>>>>>> Stashed changes
+
